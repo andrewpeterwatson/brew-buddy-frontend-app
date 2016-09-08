43a9d82043d0df0d907cd457e16ba710dc8fd57a
Cortney Lane <cdlane124@gmail.com>
2016-09-07 19:26:35 -0700

    initialized past-brew-modal files; wrote more past-brews code

--- app/view/past-brews/past-brews-controller.js
+++ app/view/past-brews/past-brews-controller.js
@@ -2,16 +2,30 @@
 
 const angular = require('angular');
 angular.module('brewBuddy')
-.controller('PastBrewsController', ['$log', '$location', 'UserSelectionsService', PastBrewsController]);
+.controller('PastBrewsController', ['$log', '$location', 'UserSelectionsService', 'entryService', PastBrewsController]);
 
-function PastBrewsController($log, $location, UserSelectionService) {
+function PastBrewsController($log, $location, UserSelectionService, entryService) {
+
+  this.pastBrews = {};
+
+  // this.fetchPastBrews = function() {
+  //   $log.debug('pastBrewsCtrl.fetchPastBrews');
+  //   entryService.fetchEntry()
+  //   .then( (entries) => {
+  //     this.pastBrews = entries;
+  //   });
+  //   .catch( () => {
+  //     return false;
+  //   })
+  // };
 
   this.fetchPastBrews = function() {
-    // fetch them brewz
+    this.pastBrews = {
+      origin: 'Antarctica',
+      brewMethod: 'Smash into some rocks',
+      flavor: 'butts',
+      date: 2016-09-08
+    };
   };
 
-  this.showPastBrewModal = function() {
-    // show dat broo
-  };
-  
 }
