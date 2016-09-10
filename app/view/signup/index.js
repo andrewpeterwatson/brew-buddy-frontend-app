920e86033a5b0528cdb42e96135aac1812ddf44e
ron oh <ronoh2001@gmail.com>
2016-09-10 15:17:50 -0700

    fixed routing from signup to signin

--- app/view/signup/index.js
+++ app/view/signup/index.js
@@ -15,7 +15,7 @@ function SignupController($log, $location, authService){
     authService.signup(this.user)
     .then( token => {
       $log.info('token', token);
-      $location.path('/user');
+      $location.path('/signin');
     })
     .catch( err => {
       $log.error(err);
