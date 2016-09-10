561a87a236c3f4bfd81c90765eab752df2eca5e4
ron oh <ronoh2001@gmail.com>
2016-09-10 15:00:14 -0700

    commented out acidiity

--- app/view/acidity/index.js
+++ app/view/acidity/index.js
@@ -1,22 +1,22 @@
-'use strict';
-
-require('./acidity.scss');
-
-const angular = require('angular');
-angular.module('brewBuddy')
-.controller('AcidityController', ['$log', '$location', 'userSelectionsService', AcidityController]);
-
-function AcidityController($log, $location, userSelectionsService){
-  $log.debug('AcidityController');
-
-
-  this.acidDesc;
-  this.intensityLevel;
-
-
-  this.nextPageFlavor = function(){
-    userSelectionsService.updateAcidity(this.acidDesc, this.intensityLevel);
-    console.log(this.acidDesc, this.intensityLevel);
-    $location.path('/user/flavor');
-  };
-}
+// 'use strict';
+//
+// require('./acidity.scss');
+//
+// const angular = require('angular');
+// angular.module('brewBuddy')
+// .controller('AcidityController', ['$log', '$location', 'userSelectionsService', AcidityController]);
+//
+// function AcidityController($log, $location, userSelectionsService){
+//   $log.debug('AcidityController');
+//
+//
+//   this.acidDesc;
+//   this.intensityLevel;
+//
+//
+//   this.nextPageFlavor = function(){
+//     userSelectionsService.updateAcidity(this.acidDesc, this.intensityLevel);
+//     console.log(this.acidDesc, this.intensityLevel);
+//     $location.path('/user/flavor');
+//   };
+// }
